74fb8f34a0e8db79a02ee2e5a573bedbb3f43ca4
Tadd Jensen <tadd@megaphonestudios.com>
2020-04-09 21:02:13 -0500

    WIP although builds, before overhaul. Give AddOns their own file (as comments allude to).

--- Objects/CommandObjects.h
+++ Objects/CommandObjects.h
@@ -3,6 +3,11 @@
 //	Vulkan Setup
 //
 // Encapsulate Command Pool/Buffer/queues.
+//		(TJ_LATER_NOTE: actually not the "queue" as that's actually device-side *receiver* of a command buffer)
+//																		and more tied to the FrameBuffer (?)
+// This class also coordinates two types of CommandBuffers: "fixed" ones
+//	recorded at initialization-time, not re-recorded, but Submitted repeatedly;
+//	and "active" CommandBuffers that re-record their commands upon every new frame.
 //
 // 1/31/19 Tadd Jensen
 //	© 0000 (uncopyrighted; use at will)
@@ -16,7 +21,6 @@
 #include "RenderPass.h"
 #include "Framebuffers.h"
 #include "GraphicsPipeline.h"
-#include "VertexBasedObject.h"
 #include "Descriptors.h"
 
 
@@ -26,52 +30,56 @@ class CommandPool
 public:
 	CommandPool(GraphicsDevice& graphicsDevice);
 	~CommandPool();
-	VkCommandPool		vkInstance;
+
+	static CommandPool& Singleton(GraphicsDevice& graphics);
+	VkCommandPool&		getVkInstance()	{ return vkInstance; }
 private:
+	static CommandPool*	pSelf;
+	VkCommandPool		vkInstance;
 	GraphicsDevice&		device;
 };
 
-struct AddOns	//TJ_TODO: need to become own separate class?
+
+enum RecordCommands {	/// i.e. Request this CommandBuffer to be recorded:
+	AT_INIT_TIME_ONLY,	///  - once at initialization-time and not re-recorded before frames.
+	UPON_EACH_FRAME		///  - repeatedly on every frame, and Reset before the next one.
+};
+
+class CommandBuffers
 {
-	friend class CommandObjects;
-	AddOns(VertexBasedObject& vertexObject, VkCommandPool& commandPool, GraphicsDevice& graphics);
-	~AddOns();
-	PrimitiveBuffer*	pVertexBuffer	= nullptr;
-	PrimitiveBuffer*	pIndexBuffer	= nullptr;
-	//UniformBuffer*	pUniformBuffer	= nullptr;	//TJ: not quite sure how to include this, TBD
+
 };
 
 
 class CommandObjects
 {
 public:
-	CommandObjects(GraphicsPipeline& pipeline, Framebuffers& framebuffers,
-				   RenderPass& renderPass, Swapchain& swapchain, GraphicsDevice& graphics,
-				   VertexBasedObject& vertexObject, Descriptors* pDescriptors);
+	CommandObjects(Framebuffers& framebuffers, RenderPass& renderPass,
+				   Swapchain& swapchain, GraphicsDevice& graphics);
 	~CommandObjects();
 
 		// MEMBERS
 private:
 	CommandPool		 commandPool;
-	AddOns	 		 addedOn;
 
 	VkCommandBuffer* commandBuffers;
 	uint32_t		 nCommandBuffers;
 
 	GraphicsDevice&	 device;
 
+	VkCommandBufferBeginInfo	beginInfo;
+
 		// METHODS
 private:
 	void createCommandPool();
 	void allocateCommandBuffers();
 	void createCommandBuffers(vector<VkFramebuffer>& framebuffers, VkExtent2D& swapChainExtent,
-							  VkRenderPass& renderPass, GraphicsPipeline& graphicsPipeline,
-							  VertexBasedObject& vertexObject, Descriptors* pDescriptors);
+							  VkRenderPass& renderPass);
+	void recordCommands(vector<VkFramebuffer>& framebuffers, VkExtent2D& swapChainExtent,
+						VkRenderPass& renderPass);
 public:
-	void Recreate(GraphicsPipeline& pipeline, Framebuffers& framebuffers,
-				  RenderPass& renderPass, Swapchain& swapchain,
-				  VertexBasedObject& pVertexObject, bool reloadMesh = false,
-				  Descriptors* pDescriptors = nullptr);
+	void Recreate(Framebuffers& framebuffers, RenderPass& renderPass,
+				  Swapchain& swapchain, bool reloadMesh = false);
 		// getters
 	const VkCommandBuffer*	CommandBuffers()	{	return commandBuffers;	}
 	uint32_t				NumCommandBuffers() {	return nCommandBuffers;	}
