5d1bc1134be1f121950fcfb20fe430d7d4610986
Tadd Jensen <tadd@megaphonestudios.com>
2020-04-23 08:49:55 -0500

    IssueBindAndDraw needs bufferIndex. Clean-up/clarify per AddOns... NOTE WORK-IN-PROGRESS UPON_EACH_FRAME/AT_INIT_TIME_ONLY KLUDGE STILL PRESENT, TO BE DEBUGGED NEXT.

--- AddOns/Renderables/FixedRenderable.cpp
+++ AddOns/Renderables/FixedRenderable.cpp
@@ -2,44 +2,37 @@
 // FixedRenderable.cpp
 //	VulkanModule AddOns
 //
+// See header file comment for overview.
+//
 // Created 3/26/20 by Tadd
 //	© 2020 Megaphone Studios
 //
 #include "FixedRenderable.h"
 #include "PrimitiveBuffer.h"
 
-// instantiate the otherwise-abstract static
-vector<iRenderable*>	Renderables::pRenderables;
-
 
 FixedRenderable::FixedRenderable(Renderable& renderable, VulkanSetup& vulkan, iPlatform& platform)
 	:	iRenderable(renderable, vulkan, platform)
 { }
-/*FixedRenderable::~FixedRenderable()	// ~iRenderable() should be automatic
-{
-	//iRenderable::~iRenderable();	//TJ: necessary? or automatic?
-}*/
 
 
-void FixedRenderable::IssueBindAndDrawCommands(VkCommandBuffer& commandBuffer)
+void FixedRenderable::IssueBindAndDrawCommands(VkCommandBuffer& commandBuffer, int bufferIndex)
 {
 	vkCmdBindPipeline(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS, pipeline.getVkPipeline());
 
-	//if (pDescriptors) // if (addedOn.pUniformBuffer)
 	if (descriptors.exist())
 		vkCmdBindDescriptorSets(commandBuffer, VK_PIPELINE_BIND_POINT_GRAPHICS,
 								pipeline.getPipelineLayout(), 0, 1,
-								&descriptors.getSets()[0], 0, nullptr);
-								 //TJ_TODO_IMPORTANT! ^^^ THIS NEEDS TO BE THE BUFFER INDEX?
+								&descriptors.getSets()[bufferIndex], 0, nullptr);
 
 	if (addOns.pVertexBuffer) {
-		VkBuffer vertexBuffers[] = { addOns.pVertexBuffer->getVkBuffer() };
+		VkBuffer vertexBuffers[] = { addOns.pVertexBuffer->getVk() };
 		VkDeviceSize offsets[]	 = { 0 };
 
 		vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers, offsets);
 	}
 	if (addOns.pIndexBuffer) {
-		vkCmdBindIndexBuffer(commandBuffer, addOns.pIndexBuffer->getVkBuffer(),
+		vkCmdBindIndexBuffer(commandBuffer, addOns.pIndexBuffer->getVk(),
 							 0, IndexBufferIndexTypeEnum);
 
 		vkCmdDrawIndexed(commandBuffer, vertexObject.indexCount, vertexObject.instanceCount,
