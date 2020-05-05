d02d556f73c4d1f090318a0f60e60e1c4496d170
Tadd Jensen <tadd@megaphonestudios.com>
2020-05-05 13:45:16 -0500

    Add stubs for GUI unneeded.

---
+++ Platform/GUISystem/imgui_impl_sdl.cpp
@@ -0,0 +1,15 @@
+//
+// imgui_impl_sdl.cpp
+//	Dummy placeholder for Dear ImGui or other GUI system.
+//
+// See header file comment for overview.
+//
+// Created 3/17/20 by Tadd Jensen
+//	© 0000 (uncopyrighted; use at will)
+//
+#include "imgui_impl_sdl.h"
+
+
+void ImGui_ImplSDL2_InitForVulkan(SDL_Window* pWindow)	{ }
+
+void ImGui_ImplSDL2_NewFrame(SDL_Window* pWindow)	{ }
