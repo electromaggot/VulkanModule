7b0ff6efcf4a661b7a974a9cce6e0f3ba157cddd
Tadd Jensen <tadd@megaphonestudios.com>
2023-04-05 20:25:50 -0500

    Grow UserInput support, adding new press types and mousewheel/pinch to zoom.

--- Platform/ControlScheme/iControlScheme.h
+++ Platform/ControlScheme/iControlScheme.h
@@ -10,6 +10,14 @@
 //	a more complex object, whether 2D (sprite) or 3D (model), acting on such objects'
 //	custom fields, for instance a velocity vector, or calling a FireShot() method.
 //
+// As for Mobile vs. Desktop platforms, a ControlScheme can either handle them
+//	separately, or try to "merge" their handling as possible; there's much overlap
+//	between the two.  However for some things, there's no equivalent - for instance
+//	"mouse hover" is impossible to mimic on Mobile.  Also, keyboard input will never
+//	be equal on Mobile, which of course requires the pop-up virtual keyboard, so can
+//	never emulate, for example, directional/strafing controls like the WASD keys.
+//	Standalone Game Controller support: a whole other story + its own ControlScheme.
+//
 // Created 15-Sep-2018 by Tadd Jensen
 //	© 0000 (uncopyrighted; use at will)
 //
@@ -22,9 +30,17 @@ class iControlScheme
 public:
 	iControlScheme() { }
 
-	virtual void handlePrimaryPressAndDrag(int toX, int toY)	{ }
-	virtual void handlePrimaryPressDown(int atX, int atY)		{ }
+	virtual void handlePrimaryPressAndDrag(int toX, int toY)	{ }		// "Press" means either mouse
+	virtual void handlePrimaryPressDown(int atX, int atY)		{ }		//	 button (click) or touch (tap).
 	virtual void handlePrimaryPressUp(int atX, int atY)			{ }
+	virtual void handleSecondaryPressDown(int atX, int atY)		{ }		// "Secondary" == right mouse
+	virtual void handleSecondaryPressUp(int atX, int atY)		{ }		//	 button or 2-finger touch.
+	virtual void handleTertiaryPressDown(int atX, int atY)		{ }		// "Tertiary" == mouse wheel
+	virtual void handleTertiaryPressUp(int atX, int atY)		{ }		//	 click or 3-finger touch.
+
+	virtual void handleMouseWheel(int spunX, int spunY)			{ }		// "can" assume zooming via mouse.
+	virtual void handlePinchSpread(float amount)				{ }		// Always means zoom via touch.
+	virtual void handleTwoFingerTwist(float angle)				{ }
 };
 
 #endif	// iControlScheme_h
