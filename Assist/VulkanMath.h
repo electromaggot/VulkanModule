3795f1d2468b3246392deb80630aa08367602f9b
Tadd Jensen <tadd@megaphonestudios.com>
2023-09-07 13:52:25 -0500

    Now exclude GLM_FORCE_LEFT_HANDED (and cater matrices explicitly for Vulkan models).

--- Assist/VulkanMath.h
+++ Assist/VulkanMath.h
@@ -26,10 +26,10 @@ enum CoordinateSystem {
 #else
 	// (Make sure glm's base directory is in your project's header paths!)
 
-	//#define GLM_FORCE_LEFT_HANDED		// for Vulkan, opposed to OpenGL!
+	//#define GLM_FORCE_LEFT_HANDED			// See note at file's end for why this is now excluded...
 	#define GLM_FORCE_RADIANS
 	#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
-	#define GLM_FORCE_DEPTH_ZERO_TO_ONE		//TJ_TODO: TBD: for Vulkan only?
+	//#define GLM_FORCE_DEPTH_ZERO_TO_ONE	//	...and this too, for now.
 	#include <glm/glm.hpp>
 	#include <glm/gtc/matrix_transform.hpp>
 
@@ -39,3 +39,18 @@ enum CoordinateSystem {
 
 
 #endif // VulkanMath_h
+
+
+// Despite our codebase being Vulkan-centric, GLM_FORCE_LEFT_HANDED does not seem
+//	necessary because of the way that glm::lookAt() and glm:perspective() are called,
+//	then the Projection matrix specifically altered or Clip Space correction applied.
+//	LunarG's Vulkan sample code does this, acting on models that appear Right-Handed
+//	and have Clockwise-wound front-facing triangles.
+// At the same time, GLM_FORCE_LEFT_HANDED seems to invoke variants of the aforenamed
+//	methods, such as lookAtLH() vs. lookAtRH() or perspectiveLH() vs. perspectiveRH().
+//	Our engine allows models of either LH ("designed for Vulkan") or RH ("designed
+//	for OpenGL") based on the MODELED_FOR_VULKAN Customizer.h flag, which can
+//	selectively call either methods at run-time.  However GLM_FORCE_LEFT_HANDED
+//	is a compile-time #define, so henceforth applies to all rendering.
+// Therefore GLM_FORCE_LEFT_HANDED is excluded and its effect otherwise
+//	applied by explicit alternative operations just mentioned.
