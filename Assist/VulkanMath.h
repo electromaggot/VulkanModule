2be9ce79b0f33fee0227211b9682475a4ccb2d07
Tadd Jensen <tadd@megaphonestudios.com>
2023-11-08 20:20:12 -0600

    Preparation to later handle modeled-for-OpenGL 3D objects.

--- Assist/VulkanMath.h
+++ Assist/VulkanMath.h
@@ -23,6 +23,7 @@
 	//#define GLM_FORCE_LEFT_HANDED		// for Vulkan, opposed to OpenGL!
 	#define GLM_FORCE_RADIANS
 	#define GLM_FORCE_DEFAULT_ALIGNED_GENTYPES
+	#define GLM_FORCE_DEPTH_ZERO_TO_ONE
 	#include <glm/glm.hpp>
 	#include <glm/gtc/matrix_transform.hpp>
 
