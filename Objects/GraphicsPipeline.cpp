52eeeca639c6d6f6c54c830772cba7030951c60a
R&D User <ruser@localhost>
2025-10-03 19:55:16 -0500

    Enable alpha_blending (while adding billboard support). Also help with procedural texture generation.

--- Objects/GraphicsPipeline.cpp
+++ Objects/GraphicsPipeline.cpp
@@ -120,14 +120,15 @@ void GraphicsPipeline::create(ShaderModules& shaderModules, VertexAbstract* pVer
 		.maxDepthBounds			= 1.0f*/
 	};
 
+	// Alpha blending: disabled by default, enabled via ALPHA_BLENDING customizer flag
 	VkPipelineColorBlendAttachmentState colorBlendAttachment = {
-		.blendEnable		 = VK_FALSE,
-		/*.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA,
+		.blendEnable		 = customize & ALPHA_BLENDING ? VK_TRUE : VK_FALSE,
+		.srcColorBlendFactor = VK_BLEND_FACTOR_SRC_ALPHA,
 		.dstColorBlendFactor = VK_BLEND_FACTOR_ONE_MINUS_SRC_ALPHA,
 		.colorBlendOp		 = VK_BLEND_OP_ADD,
 		.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE,
 		.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO,
-		.alphaBlendOp		 = VK_BLEND_OP_ADD,*/
+		.alphaBlendOp		 = VK_BLEND_OP_ADD,
 		.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT
 						| VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT
 	};
