28630b11d9fa3c33a8ba5198182b5a51a3c1647e
Tadd <tadd@megaphonestudios.com>
2026-08-27 12:42:34 -0500

    Generic (decrufted) example window sizing for TestHarness.

--- TestHarness/include/AppConstants.h
+++ TestHarness/include/AppConstants.h
@@ -31,29 +31,17 @@ extern struct Constants
 	const StrPtr CompanyName		= "GitHubProject";
 	const StrPtr ProjectName		= "HelloTriangle";
 
-#define COMPARE_AGAINST		WINDOW_5x4		// These comparison cases are for
-#if COMPARE_AGAINST == SHADERTOY_16x9		//	test and are all temporary!
-	const int DefaultWindowWidth	= 640;
-	const int DefaultWindowHeight	= 360;
-#elif COMPARE_AGAINST == IPHONE_X
-	const int DefaultWindowWidth	= 812;
-	const int DefaultWindowHeight	= 375;
-#elif COMPARE_AGAINST == WINDOW_5x4
-	const int DefaultWindowWidth	= 1280;
-	const int DefaultWindowHeight	= 1024;
-#else				 // WINDOW_16x9
-	const int DefaultWindowWidth	= 1280;
-	const int DefaultWindowHeight	= 720;
-#endif
+	const int DefaultWindowWidth	= 640;			// a generic 4 × 3 aspect ratio
+	const int DefaultWindowHeight	= 480;
 	// NOTE on WINDOW SIZE - which, even for a full-screen app on a mobile device, is still
 	//						 considered its "display window" - the surface the app renders to...
-	// The above define some starting sizes (and as you can see, per-platform), but as for the
-	//	actual "window size" variables themselves - and their definitive value* - those remain
-	//	PLATFORM-DEPENDENT, therefore reside in the iPlatform and OS Abstraction classes.
-	// * - window size may subsequently change from values above, if app implements a means to
+	// The above defines one starting size (a very small one) satisfying this test harness, but
+	//	may not be platform-compatible.  Actual "window size" variables - and their values* -
+	//	remain PLATFORM-DEPENDENT therefore reside in the iPlatform and OS Abstraction classes.
+	// * - window size may subsequently change from value above, if app implements a means to
 	//	save/retrieve size, or as user interacts via drag-resize, maximize, rotate device, etc.
 
-	const int MaxSaneScreenWidth	= 7680 * 2;		// 8K x 2 values. Simply sanity-
+	const int MaxSaneScreenWidth	= 7680 * 2;		// 8K * 2 values.  Simply sanity-
 	const int MaxSaneScreenHeight	= 4320 * 2;		//	check on pixel-related ranges.
 
 	//VkClearColorValue DefaultClearColor = { { 0.0f, 0.0f, 0.0f, 1.0f } };		// Black
